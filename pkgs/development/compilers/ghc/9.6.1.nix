24ce825b11d775c1c188c81375dc6c658be8d9ac
sternenseemann <sternenseemann@systemli.org>
2023-01-15 21:23:21 +0100

    haskell.compiler.ghc961: init at 9.6.0.20230111
    xhtml seems to be built unconditionally now which is at least one thing
    improved by hadrian.

---
+++ pkgs/development/compilers/ghc/9.6.1.nix
@@ -0,0 +1,5 @@
+import ./common-hadrian.nix rec {
+  version = "9.6.0.20230111";
+  url = "https://downloads.haskell.org/~ghc/9.6.1-alpha1/ghc-${version}-src.tar.xz";
+  sha256 = "c9647f22d8858d43c4a654ae1ef4b94660337f5658417d6344dca0d3946c6945";
+}
