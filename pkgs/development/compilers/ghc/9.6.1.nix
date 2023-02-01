eadf09bbd4f3bb4c2eafed70686dee45acde00b7
sternenseemann <sternenseemann@systemli.org>
2023-02-01 11:24:01 +0100

    haskell.compiler.ghc961: 9.6.0.20230111 -> 9.6.0.20230128

--- pkgs/development/compilers/ghc/9.6.1.nix
+++ pkgs/development/compilers/ghc/9.6.1.nix
@@ -1,5 +1,5 @@
 import ./common-hadrian.nix rec {
-  version = "9.6.0.20230111";
-  url = "https://downloads.haskell.org/~ghc/9.6.1-alpha1/ghc-${version}-src.tar.xz";
-  sha256 = "c9647f22d8858d43c4a654ae1ef4b94660337f5658417d6344dca0d3946c6945";
+  version = "9.6.0.20230128";
+  url = "https://downloads.haskell.org/~ghc/9.6.1-alpha2/ghc-${version}-src.tar.xz";
+  sha256 = "531f58dc0e352987a637095c18252e5d6fef260969eb9cb3d8447888b0922da2";
 }
