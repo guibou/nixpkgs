b90eee330097555bde049551d621a96b7dfe5e01
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2023-02-14 18:35:08 +0100

    haskell.compiler.ghc961: 9.6.0.20230128 -> 9.6.0.20230210

--- pkgs/development/compilers/ghc/9.6.1.nix
+++ pkgs/development/compilers/ghc/9.6.1.nix
@@ -1,5 +1,5 @@
 import ./common-hadrian.nix rec {
-  version = "9.6.0.20230128";
-  url = "https://downloads.haskell.org/~ghc/9.6.1-alpha2/ghc-${version}-src.tar.xz";
-  sha256 = "531f58dc0e352987a637095c18252e5d6fef260969eb9cb3d8447888b0922da2";
+  version = "9.6.0.20230210";
+  url = "https://downloads.haskell.org/~ghc/9.6.1-alpha3/ghc-${version}-src.tar.xz";
+  sha256 = "sha256-3vSnSFFiJm6R70YA38yoQki+1kFHF91+66za2WogT0c=";
 }
